b8e1278a1538378391d6114a9b7d3b333a5256e6
magobasti <82623546+magobasti@users.noreply.github.com>
2022-12-31 15:26:51 -0500

    Pagina About completada y estilisada

--- src/components/Screens/About.jsx
+++ src/components/Screens/About.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <img className='rectangulo-foto' src="./Images/foto.jpg"/>
+      <div className='texto-neto'>
+      <p className='nombre'>Adrian Rafael Bastidas Moya</p>
+      <p className='texto-about'>Hi, I’m a Full-Stack Developer and Mechatronics Engineer, bla bla bla Hi, 
+        I’m a Full-Stack Developer and Mechatronics Engineer, bla bla bla Hi, 
+        I’m a Full-Stack Developer and Mechatronics Engineer, bla bla bla Hi, 
+        I’m a Full-Stack Developer and Mechatronics Engineer, bla bla bla</p>
+      </div>
+    </div>
   )
 }
 
