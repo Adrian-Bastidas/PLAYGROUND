cb1376a02e350829446245fc20bb28ae4ff3cf9e
magobasti <82623546+magobasti@users.noreply.github.com>
2022-12-31 13:53:06 -0500

    Calculadora de IMC estilizada y mejorada

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,25 @@ import Contact from './components/Screens/Contact';
 import Footer from './components/Footer';
 
 function App() {
+  const tarjeta={
+    bajoPeso:[18.5,"Bajo Peso"],
+    adecuado:[18.5, 24.9,"Adecuado"],
+    sobrepeso:[25.0,29.9,"Sobrepeso"],
+    obesidadG1:[30.0,34.9,"Obesidad grado 1"],
+    obesidadG2:[35.0,39.9,"Obesidad grado 2"],
+    obesidadG3:[40.0,"Obesidad grado 3"],
+    minimokg:40,
+    maximokg:180,
+    minimocm:120,
+    maximocm:230,
+  }
   return (
     <div className="App">
             <Router>
       <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/Imc" element={<Imc/>}/>
+          <Route path="/Imc" element={<Imc datos={tarjeta}/>}/>
           <Route path="/Age" element={<Age/>}/>
           <Route path="/Clock" element={<Clock/>}/>
           <Route path="/About" element={<About/>}/>
